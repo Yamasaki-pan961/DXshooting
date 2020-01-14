fd12ee080e512c0c911cee42b2dc6a1962da389e
Y Hata <hata@tsuyama.kosen-ac.jp>
2020-01-14 13:41:45 +0900

    第11週で使用するインタフェースを作成した。

---
+++ DXShooting/DXShooting/IRectBounds.cs
@@ -0,0 +1,29 @@
+﻿
+namespace DXShooting
+{
+    /// <summary>
+    /// 四角形の領域を表すインタフェース。
+    /// </summary>
+    public interface IRectBounds
+    {
+        /// <summary>
+        /// 四角形領域の右上の頂点のX座標を返す。
+        /// </summary>
+        /// <returns>右上頂点のX座標値。</returns>
+        int GetNorthEastX();
+        /// <summary>
+        /// 四角形領域の右上の頂点のY座標を返す。
+        /// </summary>
+        /// <returns>右上頂点のY座標値。</returns>
+        int GetNorthEastY();
+        /// <summary>
+        /// 四角形領域の左下の頂点のX座標を返す。
+        /// </summary>
+        /// <returns>右上頂点のX座標値。</returns>
+        int GetSouthWestX();
+        /// <summary>
+        /// 四角形領域の左下の頂点のY座標を返す。
+        /// <returns>左下頂点のY座標値。</returns>
+        int GetSouthWestY();
+    }
+}
